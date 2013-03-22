9714a8fab252bc8e8d6079706b0e1b4cadafc3e9
marcusesa <marcusesa@gmail.com>
2013-03-22 16:33:22 -0300

    Adicionando os testes dos  controllers.

---
+++ src/Study/AspirinaBundle/Tests/Controller/LoginControllerTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Study\AspirinaBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class LoginControllerTest extends WebTestCase
+{
+    public function testLogin()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/login');
+    }
+
+}
