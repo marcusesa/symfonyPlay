4208c9654b445e5bf49f509de9943ce8e9a2a2ae
marcusesa <marcusesa@gmail.com>
2013-03-22 16:32:39 -0300

    Adicionando o controller .

---
+++ src/Study/AspirinaBundle/Controller/LoginController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Study\AspirinaBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\SecurityContext;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class LoginController extends Controller
+{
+    /**
+     * @Route("/login", name="login")
+     * @Template()
+     */
+    public function loginAction()
+    {
+        $request = $this->getRequest();
+        $session = $request->getSession();
+
+        // get the login error if there is one
+        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(
+                SecurityContext::AUTHENTICATION_ERROR
+            );
+        } else {
+            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContext::AUTHENTICATION_ERROR);
+        }
+
+        return array(
+                // last username entered by the user
+                'last_username' => $session->get(SecurityContext::LAST_USERNAME),
+                'error'         => $error,
+        );
+    }
+    
+    /**
+     * @Route("/login_check", name="login_check")
+     * @Template()
+     */
+    public function loginCheckAction()
+    {
+        
+    }
+    
+    /**
+     * @Route("/logout", name="logout")
+     * @Template()
+     */
+    public function logoutAction()
+    {
+        
+    }
+
+}
