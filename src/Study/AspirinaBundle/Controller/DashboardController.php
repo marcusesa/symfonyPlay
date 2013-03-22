480ee052671eeaf4b799e055be5eb31298391458
marcusesa <marcusesa@gmail.com>
2013-03-22 16:32:30 -0300

    Adicionando o controller Dashboard.

---
+++ src/Study/AspirinaBundle/Controller/DashboardController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Study\AspirinaBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class DashboardController extends Controller
+{
+    /**
+     * @Route("/")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        return array();
+    }
+
+}
