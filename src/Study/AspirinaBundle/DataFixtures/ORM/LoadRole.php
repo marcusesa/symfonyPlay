9268214ac796004b601744cbc7117f4f4de37f89
marcusesa <marcusesa@gmail.com>
2013-03-26 15:23:32 -0300

    Atualizando as datafixtures para carregar a tabela role.

--- src/Study/AspirinaBundle/DataFixtures/ORM/LoadRole.php
+++ src/Study/AspirinaBundle/DataFixtures/ORM/LoadRole.php
@@ -4,9 +4,10 @@ namespace Study\AspirinaBundle\DataFixtures\ORM;
 
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Study\AspirinaBundle\Entity\Role;
 
-class LoadRoleData implements FixtureInterface
+class LoadRoleData implements FixtureInterface, OrderedFixtureInterface
 {    
     /**
      * {@inheritDoc}
@@ -14,10 +15,30 @@ class LoadRoleData implements FixtureInterface
     public function load(ObjectManager $manager)
     {
         $role = new Role();
-        $role->setName('Admin');
+        $role->setName('Administrator');
         $role->setRole('ROLE_ADMIN');
-        
         $manager->persist($role);
+        
+        $role = new Role();
+        $role->setName('Typist');
+        $role->setRole('ROLE_TYPIST');
+        $manager->persist($role);
+        
+        $role = new Role();
+        $role->setName('Coordinator');
+        $role->setRole('ROLE_COORDINATOR');
+        $manager->persist($role);
+        
+        $role = new Role();
+        $role->setName('Statistician');
+        $role->setRole('ROLE_STATISTICIAN');
+        $manager->persist($role);
+        
         $manager->flush();
-    }    
+    }
+
+    public function getOrder()
+    {
+        return 1;
+    }
 }
