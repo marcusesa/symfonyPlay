076cccddfbb1462d99cd22ab6b02225bc431c2e3
marcusesa <marcusesa@gmail.com>
2013-03-27 09:47:30 -0300

    Adicionando o SubjectController.

---
+++ src/Study/AspirinaBundle/Controller/SubjectController.php
@@ -0,0 +1,210 @@
+<?php
+
+namespace Study\AspirinaBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Study\AspirinaBundle\Entity\Subject;
+use Study\AspirinaBundle\Form\SubjectType;
+
+/**
+ * Subject controller.
+ *
+ * @Route("/subject")
+ */
+class SubjectController extends Controller
+{
+    /**
+     * Lists all Subject entities.
+     *
+     * @Route("/", name="subject")
+     * @Method("GET")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entities = $em->getRepository('AspirinaBundle:Subject')->findAll();
+
+        return array(
+            'entities' => $entities,
+        );
+    }
+
+    /**
+     * Creates a new Subject entity.
+     *
+     * @Route("/", name="subject_create")
+     * @Method("POST")
+     * @Template("AspirinaBundle:Subject:new.html.twig")
+     */
+    public function createAction(Request $request)
+    {
+        $entity  = new Subject();
+        $form = $this->createForm(new SubjectType(), $entity);
+        $form->bind($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('subject_show', array('id' => $entity->getId())));
+        }
+
+        return array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        );
+    }
+
+    /**
+     * Displays a form to create a new Subject entity.
+     *
+     * @Route("/new", name="subject_new")
+     * @Method("GET")
+     * @Template()
+     */
+    public function newAction()
+    {
+        $entity = new Subject();
+        $form   = $this->createForm(new SubjectType(), $entity);
+
+        return array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        );
+    }
+
+    /**
+     * Finds and displays a Subject entity.
+     *
+     * @Route("/{id}", name="subject_show")
+     * @Method("GET")
+     * @Template()
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('AspirinaBundle:Subject')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Subject entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+
+        return array(
+            'entity'      => $entity,
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
+
+    /**
+     * Displays a form to edit an existing Subject entity.
+     *
+     * @Route("/{id}/edit", name="subject_edit")
+     * @Method("GET")
+     * @Template()
+     */
+    public function editAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('AspirinaBundle:Subject')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Subject entity.');
+        }
+
+        $editForm = $this->createForm(new SubjectType(), $entity);
+        $deleteForm = $this->createDeleteForm($id);
+
+        return array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
+
+    /**
+     * Edits an existing Subject entity.
+     *
+     * @Route("/{id}", name="subject_update")
+     * @Method("PUT")
+     * @Template("AspirinaBundle:Subject:edit.html.twig")
+     */
+    public function updateAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('AspirinaBundle:Subject')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Subject entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+        $editForm = $this->createForm(new SubjectType(), $entity);
+        $editForm->bind($request);
+
+        if ($editForm->isValid()) {
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('subject_edit', array('id' => $id)));
+        }
+
+        return array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
+
+    /**
+     * Deletes a Subject entity.
+     *
+     * @Route("/{id}", name="subject_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, $id)
+    {
+        $form = $this->createDeleteForm($id);
+        $form->bind($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $entity = $em->getRepository('AspirinaBundle:Subject')->find($id);
+
+            if (!$entity) {
+                throw $this->createNotFoundException('Unable to find Subject entity.');
+            }
+
+            $em->remove($entity);
+            $em->flush();
+        }
+
+        return $this->redirect($this->generateUrl('subject'));
+    }
+
+    /**
+     * Creates a form to delete a Subject entity by id.
+     *
+     * @param mixed $id The entity id
+     *
+     * @return Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm($id)
+    {
+        return $this->createFormBuilder(array('id' => $id))
+            ->add('id', 'hidden')
+            ->getForm()
+        ;
+    }
+}
