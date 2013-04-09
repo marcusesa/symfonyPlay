d0c0a1f9c44143df80d3c61028cfa607149a19fd
marcusesa <marcusesa@gmail.com>
2013-04-09 17:14:04 -0300

    Adicionando bundle para pdf na app.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new Study\AspirinaBundle\AspirinaBundle(),
+            new Spraed\PDFGeneratorBundle\SpraedPDFGeneratorBundle(),            
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
