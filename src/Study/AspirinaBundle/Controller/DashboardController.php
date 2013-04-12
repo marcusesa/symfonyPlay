36e73a21424280d5fa62ba9b3694cc688c37ef0c
marcusesa <marcusesa@gmail.com>
2013-04-12 17:51:31 -0300

    Alterando o Dashboard.

--- src/Study/AspirinaBundle/Controller/DashboardController.php
+++ src/Study/AspirinaBundle/Controller/DashboardController.php
@@ -9,12 +9,26 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class DashboardController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/", name="dashboard")
      * @Template()
      */
     public function indexAction()
     {
-        return array();
+        $em = $this->getDoctrine()->getManager();
+
+        $openDcfs = count($em->getRepository('AspirinaBundle:Dcf')->findBy(array('status' => 0)));
+        $closeDcfs = count($em->getRepository('AspirinaBundle:Dcf')->findBy(array('status' => 1)));
+        $totalDcfs = $openDcfs + $closeDcfs;
+        $totalSubjects = count($em->getRepository('AspirinaBundle:Subject')->findAll());
+        $totalSites = count($em->getRepository('AspirinaBundle:Site')->findAll());
+        
+        return array(
+            'openDcfs' => $openDcfs,
+            'closeDcfs' => $closeDcfs,
+            'totalDcfs' => $totalDcfs,
+            'totalSubjects' => $totalSubjects,
+            'totalSites' => $totalSites,
+        );
     }
 
 }
